1ad78b601fd5cc335de5d428c6c3a169c4e35736
Hajin Kim <enro302@naver.com>
2019-05-24 11:26:12 +0900

    #1 폼디자인,,

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -29,12 +29,84 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnCount = new System.Windows.Forms.Button();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "홍차",
+            "녹차",
+            "루이보스차",
+            "국화차"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(242, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // btnCount
+            // 
+            this.btnCount.Location = new System.Drawing.Point(12, 38);
+            this.btnCount.Name = "btnCount";
+            this.btnCount.Size = new System.Drawing.Size(242, 112);
+            this.btnCount.TabIndex = 1;
+            this.btnCount.Text = "담그기!";
+            this.btnCount.UseVisualStyleBackColor = true;
+            this.btnCount.Click += new System.EventHandler(this.btnCount_Click);
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(12, 156);
+            this.txt.Name = "txt";
+            this.txt.ReadOnly = true;
+            this.txt.Size = new System.Drawing.Size(242, 21);
+            this.txt.TabIndex = 2;
+            this.txt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "제작일 :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(266, 318);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.btnCount);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "TeaBagMaker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnCount;
+        private System.Windows.Forms.TextBox txt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer Timer;
     }
 }
 
--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -11,9 +11,42 @@ namespace TeaBagMaker
 {
     public partial class Form1 : Form
     {
+       
+        string[] SList = new string[]{"홍차", "녹차", "루이보스차", "국화차"};
+        string orgStr = "";
+        int CountOrgNum = 0; // 초기 카운터
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.SList.Length; i++)
+            {
+                this.comboBox1.Items.Add(SList[i]);
+            }
+            this.orgStr = this.txt.Text;
+            if (SList.Count() > 0)
+            {
+                this.comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCount_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
